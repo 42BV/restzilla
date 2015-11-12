c725f7189e962a2a4dd69f3e941f48259c9a0b19
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2015-11-12 13:44:50 +0100

    more config cleanup

--- src/main/java/io/restzilla/config/EnableRestConfiguration.java
+++ src/main/java/io/restzilla/config/EnableRestConfiguration.java
@@ -4,15 +4,11 @@
 package io.restzilla.config;
 
 import io.restzilla.handler.CrudHandlerMapping;
-import io.restzilla.util.ClasspathChecker;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportAware;
@@ -25,10 +21,8 @@ import org.springframework.core.type.AnnotationMetadata;
  * @since Nov 12, 2015
  */
 @Configuration
-public class EnableRestConfiguration implements ImportAware, InitializingBean, ApplicationContextAware {
+public class EnableRestConfiguration implements ImportAware, ApplicationContextAware {
     
-    private static final String SWAGGER_PACKAGE = "com.mangofactory.swagger";
-
     private static final String BASE_PACKAGE_CLASS_NAME = "basePackageClass";
 
     private ApplicationContext applicationContext;
@@ -64,26 +58,6 @@ public class EnableRestConfiguration implements ImportAware, InitializingBean, A
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        if (ClasspathChecker.isOnClasspath(SWAGGER_PACKAGE)) {
-            // Automatically register our swagger plugin when none are configured
-            if (applicationContext.getBeansOfType(com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin.class).isEmpty()) {
-                registerSwaggerPlugin();
-            }
-        }
-    }
-
-    private void registerSwaggerPlugin() throws Exception {
-        com.mangofactory.swagger.configuration.SpringSwaggerConfig springSwaggerConfig = applicationContext.getBean(com.mangofactory.swagger.configuration.SpringSwaggerConfig.class);
-        com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin swaggerSpringMvcPlugin = new io.restzilla.handler.swagger.SwaggerRestPlugin(springSwaggerConfig, crudHandlerMapping());
-        ConfigurableListableBeanFactory beanFactory = ((ConfigurableApplicationContext) applicationContext).getBeanFactory();
-        beanFactory.registerSingleton("swaggerSpringMvcPlugin", swaggerSpringMvcPlugin);
     }
 
 }
