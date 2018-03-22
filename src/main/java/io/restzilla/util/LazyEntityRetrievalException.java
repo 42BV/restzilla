6e10364bf28616bb3b6f084fed14d93a25f8d800
Arjan Vlek <arjan.vlek@42.nl>
2018-03-22 08:58:38 +0100

    Fixed NoSuchMethodError when running with the latest beanmapper-spring

---
+++ src/main/java/io/restzilla/util/LazyEntityRetrievalException.java
@@ -0,0 +1,12 @@
+package io.restzilla.util;
+
+/**
+ * LazyEntityRetrievalException is a runtime exception which is thrown when the underlying BeanMapper failed to obtain a Lazy entity
+ * In such case, we are usually outside of the Hibernate session in which the Lazy wrapper was retrieved.
+ */
+public class LazyEntityRetrievalException extends RuntimeException {
+
+    public LazyEntityRetrievalException(Exception e) {
+        super(e);
+    }
+}
